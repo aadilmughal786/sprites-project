dcaa085e7f230b26e0eee4776440dfeb2553f786
Aadil Mugal <aadilshe786@gmail.com>
2023-07-30 02:50:52 +0530

    feat : add index page

--- girl-running/scripts/script.js
+++ girl-running/scripts/script.js
@@ -3,12 +3,12 @@ const sprite = document.querySelector(".sprite");
 const speed = document.getElementById("speed-value");
 
 document.addEventListener("DOMContentLoaded", () => {
-  speed.innerHTML = `Speed: <span class="chip">${range.value}s</span>`;
+  speed.innerHTML = `Animation speed: <span class="chip">${range.value}s</span>`;
   sprite.style.animationDuration = `${range.value}s`;
 });
 
 range.addEventListener("change", (e) => {
   let value = e.target.value;
   sprite.style.animationDuration = `${value}s`;
-  speed.innerHTML = `Speed: <span class="chip">${value}s</span>`;
+  speed.innerHTML = `Animation speed: <span class="chip">${value}s</span>`;
 });
